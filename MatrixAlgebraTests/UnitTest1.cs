20ef5c5762f2addca0500561e01ff0c18fb756cd
Daniil Chuprikov <d.chuprikov@aurigma.com>
2023-04-01 21:18:51 +0700

    add tests project

---
+++ MatrixAlgebraTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MatrixAlgebraTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
