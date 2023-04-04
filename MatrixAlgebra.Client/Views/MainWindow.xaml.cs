1ad2f12fbc5fc173edbb5934e14547004b3bef5c
Kawodeda <konserva023@gmail.com>
2023-04-04 22:44:44 +0700

    add MainWindow view

---
+++ MatrixAlgebra.Client/Views/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace MatrixAlgebra.Client.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<List<string>> _values = new List<List<string>>()
+        {
+            new List<string>() { "0", "1", "2" },
+            new List<string>() { "3", "4", "5" },
+            new List<string>() { "6", "7", "8" }
+        };
+
+        public List<string> Ops => new List<string>()
+        {
+            "Add",
+            "Subtract",
+            "Multiply"
+        };
+
+        public List<List<string>> Values => _values;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
